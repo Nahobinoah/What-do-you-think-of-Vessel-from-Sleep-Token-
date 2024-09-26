5d80d8aa0ea0e0f1602f84e0ad8e99c456fdbdec
Nahobinoah <noahfairtag@gmail.com>
2024-09-26 18:09:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+Vessel = input("What do you think of Vessel from Sleep Token?")
+if Vessel == ("I want to marry him"):
+  print("Welcome to the cult")
+else:
+  print(
+      "You are not worthy of your mortal coil...\nYou suddenly hear a knock at the door. You open it and are hit in the face with a large hammer.\nYou come to, and realise that you are sitting in a Sleep Token concert.\nThis was Sleep Token's way to get you on their side. By forcing you to listen to their songs.\nBy the end of the night you become a true follower.\nIt was worth the kidnapping in your eyes."
+  )
